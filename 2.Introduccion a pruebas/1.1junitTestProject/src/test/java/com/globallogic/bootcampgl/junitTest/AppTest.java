2e289064108ea5cf263510a6bf983ae6cef9df5f
ncasanovas <nataliasolcasanovas@gmail.com>
2020-08-01 14:07:22 -0300

    Ejercicio 1.1

---
+++ 2.Introduccion a pruebas/1.1junitTestProject/src/test/java/com/globallogic/bootcampgl/junitTest/AppTest.java
@@ -0,0 +1,15 @@
+package com.globallogic.bootcampgl.junitTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+	@Test
+	void test() {
+
+		assertEquals("Test", "Test");
+
+	}
+
+}
